ae7c5e1364155a83d21971166a61f553395ccc23
Antoine <antoine.bats@gmail.com>
2026-01-14 12:01:56 +0000

    Task 6

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for d1 in range(10):
     for d2 in range(d1+1, 10):
-        print("{:d}{:d}".format(d1, d2), end=", " if d2 < 9 or d1 < 8 else "\n")
+        print(f"{d1}{d2}", end=", " if d2 < 9 or d1 < 8 else "\n")
