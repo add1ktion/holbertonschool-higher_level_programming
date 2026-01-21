724b7a189ba471e0f5368325caaec25c54d009c2
Antoine <antoine.bats@gmail.com>
2026-01-21 09:06:24 +0000

    Task 0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,3 +1,18 @@
 #!/usr/bin/python3
+"""Adds 2 integers. a and b must be integers or floats,
+otherwise raise a TypeError exception.
+a and b must be first casted to integers (if float) and return int sum."""
+
+
 def add_integer(a, b=98):
-    
+    """
+    Adds 2 integers.
+    Args: a (int, float): first number. b (int, float): second number.
+    Return: int: Sum of a + b.
+    """
+
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
