f30d9351f3a49ca707d079c03a3754ff17a71d10
Antoine <antoine.bats@gmail.com>
2026-01-22 13:03:28 +0000

    Task 0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,26 +1,15 @@
 #!/usr/bin/python3
 """Adds 2 integers.
-
 Prototype: def add_integer(a, b=98):
 - a and b must be integers or floats, otherwise raise a TypeError
-  exception with the message `a must be an integer` or `b must be an integer`
 - a and b must be first casted to integers (if they are floats)
-- Returns an integer: the addition of a and b
-- You are not allowed to import any module
-"""
+- Returns an integer: the addition of a and b"""
 
 
 def add_integer(a, b=98):
-    """
-    Adds two integers.
-
-    Args:
-        a (int, float): First number.
-        b (int, float): Second number (default 98).
-
-    Returns:
-        int: Sum of a and b.
-    """
+    """Adds two integers.
+    Args: a (int, float): First number. b (int, float): Second number (default 98).
+    Returns: int: Sum of a and b."""
 
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
