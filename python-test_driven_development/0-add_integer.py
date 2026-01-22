684090ffae70cda1e3533a17764150d5eacd50c7
Antoine <antoine.bats@gmail.com>
2026-01-22 13:19:07 +0000

    Task 0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -8,13 +8,11 @@ Prototype: def add_integer(a, b=98):
 
 def add_integer(a, b=98):
     """Adds two integers.
-    Args: a (int, float): First number. b (int, float): Second number (default 98).
+    Args: a: First number. b:Second number (default 98).
     Returns: int: Sum of a and b."""
 
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    a = int(float(a))
-    b = int(float(b))
     return int(a) + int(b)
