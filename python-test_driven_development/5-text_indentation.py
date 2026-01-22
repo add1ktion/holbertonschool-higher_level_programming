79c80ef883d9ad459d466b5ab75f923b658db420
Antoine <antoine.bats@gmail.com>
2026-01-22 08:59:16 +0000

    Task 4

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,3 +1,17 @@
 #!/usr/bin/python3
 def text_indentation(text):
-    
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    skip_space = False
+    for c in text:
+        if c in '.?:':
+            print(c, end="")
+            print()
+            skip_space = True
+            continue
+        if skip_space and c == ' ':
+            skip_space = False
+            print()
+        else:
+            print(c, end="")
