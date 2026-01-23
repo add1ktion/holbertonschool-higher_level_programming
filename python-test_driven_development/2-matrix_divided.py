3c5927d5c235a0a0d20d552164d1d63315896e61
Antoine <antoine.bats@gmail.com>
2026-01-23 08:06:03 +0000

    Task 1

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -3,45 +3,47 @@
 Divides all elements of a matrix.
 
 Prototype: def matrix_divided(matrix, div):
-- matrix must be list of lists of ints/floats, same size rows
-- div must be number (int/float), cannot be 0
-- Returns new matrix with elements / div rounded to 2 decimals
-- Raises TypeError/ZeroDivisionError on invalid input
+- matrix must be a matrix (list of lists) of integers/floats
+- Each row of the matrix must have the same size
+- div must be a number
+- div can't be equal to 0
+- Returns a new matrix
+You are not allowed to import any module
 """
-
 
 def matrix_divided(matrix, div):
     """
-    Divides matrix elements by div, returns new matrix rounded to 2dp.
+    Divides all elements of a matrix.
 
     Args:
-        matrix (list): List of lists of numbers
-        div (int, float): Divisor (non-zero)
+        matrix (list): Matrix to divide.
+        div (int, float): Divisor.
 
     Returns:
-        list: New divided matrix
+        list: New matrix with elements divided by div, rounded to 2 decimals.
     """
     msg1 = "matrix must be a matrix (list of lists) of integers/floats"
     msg2 = "Each row of the matrix must have the same size"
     msg3 = "div must be a number"
-
-    if not isinstance(matrix, list) or len(matrix) == 0:
-        raise TypeError(msg1)
-    if not isinstance(matrix[0], list):
-        raise TypeError(msg1)
-
-    first_len = len(matrix[0])
-    for row in matrix:
-        if not isinstance(row, list) or len(row) != first_len:
-            raise TypeError(msg2)
-        for el in row:
-            if not isinstance(el, (int, float)):
-                raise TypeError(msg1)
 
     if not isinstance(div, (int, float)):
         raise TypeError(msg3)
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
+    if (not isinstance(matrix, list) or 
+        len(matrix) == 0 or 
+        not all(isinstance(row, list) and len(row) > 0 for row in matrix)):
+        raise TypeError(msg1)
+
+    first_len = len(matrix[0])
+    for row in matrix:
+        if len(row) != first_len:
+            raise TypeError(msg2)
+        for el in row:
+            if not isinstance(el, (int, float)):
+                raise TypeError(msg1)
+
     new_matrix = [[round(el / div, 2) for el in row] for row in matrix]
     return new_matrix
+
