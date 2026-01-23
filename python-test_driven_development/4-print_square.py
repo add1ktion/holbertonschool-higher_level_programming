218bb4f59c9602a50d348933d424f2b64a4f9851
Antoine <antoine.bats@gmail.com>
2026-01-23 09:04:08 +0000

    Task 3

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -19,12 +19,14 @@ def print_square(size):
         TypeError: If size not integer
         ValueError: If size < 0
     """
-    if not isinstance(size, int):
+    if isinstance(size, float) and size < 0:
         raise TypeError("size must be an integer")
+
+    if not isinstance(size, int) or isinstance(size, bool):
+        raise TypeError("size must be an integer")
+
     if size < 0:
         raise ValueError("size must be >= 0")
 
     for i in range(size):
-        for j in range(size):
-            print("#", end="")
-        print()
+        print("#" * size)
