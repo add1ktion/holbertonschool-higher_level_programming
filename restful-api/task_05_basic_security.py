2ed38d93b5aabc3357f5e16ac9f88b1f99a300d1
Antoine <antoine.bats@gmail.com>
2026-02-17 17:01:03 +0000

    T5

---
+++ restful-api/task_05_basic_security.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+from flask import Flask
+from flask_httpauth import HTTPBasicAuth
+from werkzeug.security import generate_password_hash, check_password_hash
+
+app = Flask(__name__)
+auth = HTTPBasicAuth()
+
+@auth.login_required
+def get_pw(username):
+    if username in users:
+        return users.get(username)
+    return None
+
+@app.route('/')
+@auth.login_required
+def index():
+    return "Hello, {}!".format(auth.username())
+
+if __name__ == '__main__':
+    app.run()
