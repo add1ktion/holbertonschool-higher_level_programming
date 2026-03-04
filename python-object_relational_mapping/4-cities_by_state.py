94b8343ce10edf2c30b7f9094a4d7b2caceb743b
Antoine <antoine.bats@gmail.com>
2026-03-04 13:06:03 +0000

    P2T4

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -20,7 +20,7 @@ if __name__ == "__main__":
     cur.execute("""SELECT cities.id, cities.name, states.name
                 FROM cities JOIN states ON cities.state_id = states.id
                 ORDER BY cities.id ASC"""
-    )
+                )
 
     query_rows = cur.fetchall()
 
