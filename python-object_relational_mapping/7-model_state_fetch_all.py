fbd6a9145630ce3a83fec2ea319ec3c9e4a04f26
add1ktion <antoine.bats@gmail.com>
2026-03-05 14:12:07 +0100

    P2T7

---
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,13 @@
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+import sys
+
+if __name__ == "__main__":
+	engine = create_engine('mysql+mysqldb://{}:{}@127.0.0.1:3306/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+	Session = sessionmaker(bind=engine)
+	session = Session()
+
+	states = session.query(State).order_by(State.id).all()
+	for state in states:
+		print("{}: {}".format(state.id, state.name))
