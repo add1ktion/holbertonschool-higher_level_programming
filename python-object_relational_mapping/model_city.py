d34399f0899942ad067596566627fc171772dba3
add1ktion <antoine.bats@gmail.com>
2026-03-06 10:02:57 +0100

    P2T14

---
+++ python-object_relational_mapping/model_city.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Defines the City class"""
+from sqlalchemy import Column, Integer, String, ForeignKey
+from model_state import Base
+
+
+class City(Base):
+    """City class definition"""
+    __tablename__ = 'cities'
+
+    id = Column(
+        Integer,
+        autoincrement=True,
+        primary_key=True
+        )
+
+    name = Column(
+        String(128),
+        nullable=False
+        )
+
+    state_id = Column(
+        Integer,
+        ForeignKey('states.id'),
+        nullable=False
+        )
