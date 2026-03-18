18185056be85b43790b99f642c4f5e7a884947c4
add1ktion <antoine.bats@gmail.com>
2026-03-18 13:42:37 +0100

    T4

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -4,9 +4,9 @@ Write a script that prints two arguments passed to it, in the following format: 
 */
 
 if (process.argv[2] && process.argv[3]) {
-    console.log(process.argv[2] + ' is ' + process.argv[3]);
-} else if (process.argv[2] + !process.argv[3]) {
-    console.log(process.argv[2] + ' is undefined');  
+  console.log(process.argv[2] + ' is ' + process.argv[3]);
+} else if (process.argv[2] && !process.argv[3]) {
+  console.log(process.argv[2] + ' is undefined');
 } else {
-    console.log('undefined is undefined');
+  console.log('undefined is undefined');
 }
