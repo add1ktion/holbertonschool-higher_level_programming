11572d682999e4e78979a933f77c38a599d912c3
add1ktion <antoine.bats@gmail.com>
2026-03-18 13:07:34 +0100

    T1

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -3,6 +3,5 @@
 Write a JavaScript script that prints "JavaScript is amazing" in multiple languages
 */
 
-console.log("C is fun");
-console.log("Python is cool");
-console.log("JavaScript is amazing");
+const myVar = 'C is fun\n' + 'Python is cool\n' + 'JavaScript is amazing';
+console.log(myVar);
