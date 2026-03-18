7f041895ff02821b78804de2a498bd942cf58ede
add1ktion <antoine.bats@gmail.com>
2026-03-18 13:30:28 +0100

    T2

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -5,10 +5,8 @@ Write a script that prints a message depending of the number of arguments passed
 
 if (process.argv.length <= 2) {
   console.log('No argument');
-}
-else if (process.argv.length === 3) {
+} else if (process.argv.length === 3) {
   console.log('Argument found');
-}
-else {
+} else {
   console.log('Arguments found');
 }
