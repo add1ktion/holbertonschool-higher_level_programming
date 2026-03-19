d3fc68840f7269eec345a9997dc9fab05c8ff5ff
add1ktion <antoine.bats@gmail.com>
2026-03-19 09:13:34 +0100

    P2

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+/* 10-factorial.js
+Write a script that computes and prints a factorial
+*/
+
+function factorial (n) {
+  if (isNaN(n) || n === 0 || n === 1) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
+}
+
+const num = parseInt(process.argv[2]);
+console.log(factorial(num));
---
+++ javascript-warm_up/13-add.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+/* 13-add.js
+Write a function that returns the addition of 2 integers.
+*/
+
+const a = parseInt(process.argv[2]);
+const b = parseInt(process.argv[3]);
+console.log(a + b);
