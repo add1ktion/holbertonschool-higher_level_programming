015c05ae604ec219741dfa53a771d1bb2029e731
add1ktion <antoine.bats@gmail.com>
2026-03-19 09:41:47 +0100

    P2

--- javascript-warm_up/13-add.js
+++ javascript-warm_up/13-add.js
@@ -3,6 +3,6 @@
 Write a function that returns the addition of 2 integers.
 */
 
-const a = parseInt(process.argv[2]);
-const b = parseInt(process.argv[3]);
-console.log(a + b);
+exports.add = function (a, b) {
+  return a + b;
+}
